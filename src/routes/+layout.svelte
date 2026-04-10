7cd948b626b6e87e3a53fcae7da5d3ed2b59b600
Usukh-Erdene Choijamts <uujuu316@gmail.com>
2026-04-10 15:34:23 +0800

    vercel analytics, speed insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,12 @@
 <script>
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.png';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+	import { dev } from '$app/environment';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
 
+	injectAnalytics({ mode: dev ? 'development' : 'production' });
+	injectSpeedInsights();
 	let { children } = $props();
 </script>
 
